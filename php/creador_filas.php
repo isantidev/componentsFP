a26e9c6d6e25ff31e0cc22c651dfbed43bc17baa
iSantiDev <santis.sierra6@gmail.com>
2024-11-18 10:40:33 -0500

    some good changes

--- php/creador_filas.php
+++ php/creador_filas.php
@@ -9,27 +9,28 @@ $usuario->execute();
 if ($usuario->rowCount() > 0) {
     $datosUsuario = $usuario->fetchAll(PDO::FETCH_ASSOC);
     foreach ($datosUsuario as $row) {
-        $fila = '<div class="usuario_fila_contenedor">
-    <ul class="usuario_fila">
-        <li class="usuario_columna">
+        $fila = '
+        <div class="usuario_fila_contenedor">
+            <ul class="usuario_fila">
+                <li class="usuario_columna">
 
-            <img src=';
+                    <img src=';
         $fila .= '"/images/logo.jpg" alt="' . $row["nombre_completo"] . '">';
         $fila .= '</li>
-        <li class="usuario_columna">
-            <p data-texto="Nombre: ">' . $row["nombre_completo"] . '</p>
-        </li>
-        <li class="usuario_columna">
-            <p data-texto="Identificador: ">' . $row["cc_id"] . '</p>
-        </li>
-        <li class="usuario_columna columna_correo">
-            <a href="mailto:' . $row["email"] . '" data-texto="Correo: ">' . $row["email"] . '</a>
-        </li>
-        <li class="usuario_columna">
-            <a href="tel:' . $row["contacto"] . '" data-texto="Contacto: ">' . $row["contacto"] . '</p>
-        </li>
-    </ul>
-    </div>';
+                <li class="usuario_columna">
+                    <p data-texto="Nombre: ">' . $row["nombre_completo"] . '</p>
+                </li>
+                <li class="usuario_columna">
+                    <p data-texto="Identificador: ">' . $row["cc_id"] . '</p>
+                </li>
+                <li class="usuario_columna columna_correo">
+                    <a href="mailto:' . $row["email"] . '" data-texto="Correo: ">' . $row["email"] . '</a>
+                </li>
+                <li class="usuario_columna columna_contacto">
+                    <a href="tel:' . $row["contacto"] . '" data-texto="Contacto: ">' . $row["contacto"] . '</a>
+                </li>
+            </ul>
+        </div>';
         echo $fila;
     }
 }
