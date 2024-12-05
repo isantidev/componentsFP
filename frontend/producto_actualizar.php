e1d23088d68baa1375911bcd6556a0a661f421ab
iSantiDev <santis.sierra6@gmail.com>
2024-12-05 10:26:28 -0500

    Dios, dame paciencia

--- frontend/producto_actualizar.php
+++ frontend/producto_actualizar.php
@@ -6,7 +6,6 @@ $producto_info = "SELECT * FROM producto ORDER BY nombre";
 $stmt = $conn->prepare($producto_info);
 $stmt->execute();
 $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
 
 <head>
@@ -15,57 +14,55 @@ $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 <div class="producto_actualizar_wrapper">
     <div class="selected_producto_input">
-        <label for="select_producto"></label>
-        <select name="select_id_producto" id="select_producto_edit">
+        <label for="select_elemento_edit"></label>
+        <select name="select_id_producto" id="select_elemento_edit">
             <option value="" selected disabled>Seleccione un producto</option>
             <?php
             foreach ($productos as $producto) {
                 $option_producto = '<option value="' . $producto['producto_id']
-                    . '" data-nombre="' . $producto['nombre']
-                    . '" data-descripcion="' . $producto['descripcion']
-                    . '" data-precio="' . $producto['precio']
-                    . '" data-proveedor-id="' . $producto['proveedor_id']
                     . '">' . $producto['nombre'] . '</option>';
                 echo $option_producto;
             }
             ?>
         </select>
     </div>
-    <form action="../php/registrar_producto.php" method="post" class="producto_actualizar_form">
-        <div class="input_contenedor">
-            <label for="producto_id_edit">Id del Producto</label>
-            <input type="text" name="producto-id" id="producto_id_edit">
-        </div>
-        <div class="input_contenedor">
-            <label for="producto_nombre_edit">Nombre del producto</label>
-            <input type="text" name="producto-nombre" id="producto_nombre_edit">
-        </div>
-        <div class="input_contenedor">
-            <label for="producto_descripcion_edit">Descrición del Producto</label>
-            <input type="text" name="producto-descripcion" id="producto_descripcion_edit">
-        </div>
-        <div class="input_contenedor">
-            <label for="producto_precio_edit">Precio del producto</label>
-            <input type="number" name="producto-precio" id="producto_precio_edit">
-        </div>
-        <div class="input_contenedor">
-            <label for="select_proveedor">Id del Proveedor</label>
-            <select name="select-id-proveedor" id="select_proveedor_id_edit">
-                <?php
-                $sql_info_proveedor = "SELECT proveedor_id, nombre FROM proveedor";
-                $stmt_info_proveedor = $conn->prepare($sql_info_proveedor);
-                $stmt_info_proveedor->execute();
-                $proveedores = $stmt_info_proveedor->fetchAll(PDO::FETCH_ASSOC);
+    <div id="info_selected_wrapper"></div>
+</div>
+<form action="../php/registrar_producto.php" method="post" class="producto_actualizar_form">
+    <div class="input_contenedor">
+        <label for="producto_id_edit">Id del Producto</label>
+        <input type="text" name="producto-id" id="producto_id_edit">
+    </div>
+    <div class="input_contenedor">
+        <label for="producto_nombre_edit">Nombre del producto</label>
+        <input type="text" name="producto-nombre" id="producto_nombre_edit">
+    </div>
+    <div class="input_contenedor">
+        <label for="producto_descripcion_edit">Descrición del Producto</label>
+        <input type="text" name="producto-descripcion" id="producto_descripcion_edit">
+    </div>
+    <div class="input_contenedor">
+        <label for="producto_precio_edit">Precio del producto</label>
+        <input type="number" name="producto-precio" id="producto_precio_edit">
+    </div>
+    <div class="input_contenedor">
+        <label for="select_proveedor_id_edit">Id del Proveedor</label>
+        <select name="select-id-proveedor" id="select_proveedor_id_edit">
+            <?php
+            $sql_info_proveedor = "SELECT proveedor_id, nombre FROM proveedor";
+            $stmt_info_proveedor = $conn->prepare($sql_info_proveedor);
+            $stmt_info_proveedor->execute();
+            $proveedores = $stmt_info_proveedor->fetchAll(PDO::FETCH_ASSOC);
 
-                foreach ($proveedores as $proveedor) {
-                    $option_proveedor = '<option value="' . $proveedor['proveedor_id'] . '">' . $proveedor['nombre'] . '</option>';
-                    echo $option_proveedor;
-                }
-                ?>
-            </select>
-        </div>
-        <div class="input_contenedor btn-enviar">
-            <button type="submit">Actualizar Información</button>
-        </div>
-    </form>
+            foreach ($proveedores as $proveedor) {
+                $option_proveedor = '<option value="' . $proveedor['proveedor_id'] . '">' . $proveedor['nombre'] . '</option>';
+                echo $option_proveedor;
+            }
+            ?>
+        </select>
+    </div>
+    <div class="input_contenedor btn-enviar">
+        <button type="submit">Actualizar Información</button>
+    </div>
+</form>
 </div>
--- js/dinamic_page.js
+++ js/dinamic_page.js
@@ -26,3 +26,57 @@ document.addEventListener("DOMContentLoaded", async () => {
         });
     });
 });
+
+document.addEventListener("DOMContentLoaded", async () => {
+    let productos = [];
+
+    try {
+        // Solicitar los datos desde producto_info.php
+        const response = await fetch("../php/producto_info.php");
+        productos = await response.json();
+        console.log("Productos cargados:", productos);
+    } catch (error) {
+        console.error("Error al cargar los productos:", error);
+        return;
+    }
+});
+
+const producto_selected = document.getElementById("select_producto_edit");
+const detalles_contenedor = document.getElementById("info_selected_wrapper");
+
+producto_selected.addEventListener("change", () => {
+    const productoIdSelected = producto_selected.value;
+    detalles_contenedor.innerHTML = "";
+
+    const selectedProducto = productos.find(
+        (producto) => producto.producto_id === productoIdSelected
+    );
+
+    if (selectedProducto) {
+        detalles_contenedor.style.display = "block";
+        detalles_contenedor.innerHTML = `
+                <h4>${selectedProducto.producto_id} - ${
+            selectedProducto.nombre
+        }</h4>
+                <ul class="list_producto_info">
+                    <li class="producto_info_item">
+                        <p><strong>Precio:</strong> ${
+                            selectedProducto.precio
+                        }</p>
+                    </li>
+                    <li class="producto_info_item">
+                        <p><strong>Descripción:</strong> ${
+                            selectedProducto.descripcion || "No disponible"
+                        }</p>
+                    </li>
+                    <li class="producto_info_item">
+                        <p><strong>ID de Proveedor:</strong> ${
+                            selectedProducto.proveedor_id
+                        }</p>
+                    </li>
+                </ul>
+            `;
+    } else {
+        detalles_contenedor.style.display = "none";
+    }
+});
